2741fe735dbd275ed0c48914501ba721a625eb9f
s-panatulag <s-panatulag@49S50024532.it.bsd405.org>
2018-12-04 22:33:08 -0800

    Returns third operand

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -1,38 +1,27 @@
+import java.util.*;
 public class FracCalc {
 
-    /**
-     * Prompts user for input, passes that input to produceAnswer, then outputs the result.
-     * @param args - unused
-     */
     public static void main(String[] args) 
     {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-        // Checkpoint 1: Create a Scanner, read one line of input, pass that input to produceAnswer, print the result.
-        // Checkpoint 2: Accept user input multiple times.
+        Scanner console = new Scanner (System.in); 
+        System.out.print ("Input your equation  ");
+        String input = console.nextLine ();        
+        String last = produceAnswer (input);
+        System.out.println (last);
+        
+       /* while (!(input.equals("-1"))) {
+        	String answer = produceAnswer (input);
+        	System.out.println(answer);
+        	System.out.print ("Input your equation (print -1 to quit) ");
+        	input = console.nextLine ();
+        }
+        */
+    }
+    public static String produceAnswer(String input) {
+    	String equation[] = input.split(" ");
+    	return equation[equation.length-1];
     }
     
-    /**
-     * produceAnswer - This function takes a String 'input' and produces the result.
-     * @param input - A fraction string that needs to be evaluated.  For your program, this will be the user input.
-     *      Example: input ==> "1/2 + 3/4"
-     * @return the result of the fraction after it has been calculated.
-     *      Example: return ==> "1_1/4"
-     */
-    public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input
-        // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
-        // Checkpoint 2: Return the second operand as a string representing each part.
-        //               Example "4/5 * 1_2/4" returns "whole:1 numerator:2 denominator:4".
-        // Checkpoint 3: Evaluate the formula and return the result as a fraction.
-        //               Example "4/5 * 1_2/4" returns "6/5".
-        //               Note: Answer does not need to be reduced, but it must be correct.
-        // Final project: All answers must be reduced.
-        //               Example "4/5 * 1_2/4" returns "1_1/5".
-        
-        return "";
-    }
-
     // TODO: Fill in the space below with helper methods
     
     /**
