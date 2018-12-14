3ceb58305506dcb82545bc558651ecbbccfba04d
s-panatulag <s-panatulag@49S50024532.it.bsd405.org>
2018-12-13 23:20:30 -0800

    Finished project?

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -6,76 +6,71 @@ public class FracCalc {
         System.out.print ("Input your equation (type 'quit' to quit): ");
         String input = console.nextLine ();      
         while (!(input.equals("quit"))) {
-        	produceAnswer (input); //calls produceAnswer 
+        	String answer = produceAnswer (input); //calls produceAnswer
+        	String solution[] = answer.split(" ");
+        	if (solution.length == 1) { //if the answer is an integer
+        		System.out.println(solution[0]);
+        	}
+        	if (solution.length == 2) {//if the answer is a reduced fraction
+        		System.out.println(solution[0] + "/" + solution[1]);
+        	}
+        	if (solution.length == 3) {//if the answer is a mixed fraction
+        		System.out.println(solution[0] + "_" + solution[1] + "/" + solution[2]);
+        	}
         	System.out.print ("Input your equation (print 'quit' to quit): ");
         	input = console.nextLine ();
     	}	        
     }
     
-    public static String produceAnswer(String input) {
+    public static String produceAnswer (String input) {
     	String equation[] = input.split(" "); //splits the input into two operands and one operator and makes an array with them
 	    int [] operand1 = operandHolder (equation[0]); //finds the num, denom, and whole for the first operand and puts into array 'operand1'
 	    int [] operand2 = operandHolder(equation[2]); //finds the num, denom, and whole for the second operand and puts into array 'operand2'
-	    if (operand1[0] > 0) { //if operand1 is a mixed fraction
+	    if (operand1[0] != 0) { //if operand1 is a mixed fraction
 	    	operand1 = improper(operand1); //makes operand1 an improper fraction
 	    }
-	    if (operand2[0] > 0) { //if operand2 is a mixed fraction
+	    if (operand2[0] != 0) { //if operand2 is a mixed fraction
 	    	operand2 = improper(operand2); //makes operand2 an improper fraction
 	    }
 	    if (equation[1].equals("*")) { //checks whether the equation needs to multiply
 	    	int [] answer = multiply (operand1, operand2);
-	    	reduce (answer);
-	    	if (answer[2] == 1) { //if the answer is an integer
-	    		System.out.println(answer[1]);
-	    	}
-	    	else if (answer[0] > 0) {//if the fraction is improper
-	    		System.out.println(answer[0] + "_" + answer[1] + "/" + answer[2]);
-	    	}
-	    	else {
-	    		System.out.println(answer[1] + "/" + answer[2]);
-	    	}
+	    	return returner (answer);
 	    }
 	    if (equation[1].equals("/")) { //checks whether the equation needs to divide
 	    	int [] answer = divide (operand1, operand2);
-	    	reduce (answer);
-	    	if (answer[2] == 1) { //if the answer is an integer
-	    		System.out.println(answer[1]);
-	    	}
-	    	else if (answer[0] > 0) {//if the fraction is improper
-	    		System.out.println(answer[0] + "_" + answer[1] + "/" + answer[2]);
-	    	}
-	    	else {
-	    		System.out.println(answer[1] + "/" + answer[2]);
-	    	}
+	    	return returner (answer);
 	    }
 	    if (equation[1].equals("+")) { //checks whether the equation needs to add
 	    	int [] answer = addition (operand1, operand2);
-	    	reduce (answer);
-	    	if (answer[2] == 1) { //if the answer is an integer
-	    		System.out.println(answer[1]);
-	    	}
-	    	else if (answer[0] > 0) {//if the fraction is improper
-	    		System.out.println(answer[0] + "_" + answer[1] + "/" + answer[2]);
-	    	}
-	    	else {
-	    		System.out.println(answer[1] + "/" + answer[2]);
-	    	}
+	    	return returner (answer);
 	    }
 	    if (equation[1].equals("-")) { //checks whether the equation needs to subtract
 	    	int [] answer = subtraction (operand1, operand2);
-	    	reduce (answer);
-	    	if (answer[2] == 1) { //if the answer is an integer
-	    		System.out.println(answer[1]);
-	    	}
-	    	else if (answer[0] > 0) {//if the fraction is improper
-	    		System.out.println(answer[0] + "_" + answer[1] + "/" + answer[2]);
-	    	}
-	    	else {
-	    		System.out.println(answer[1] + "/" + answer[2]);
-	    	}
+	    	return returner (answer);
+	    }
+	    else {
+	    	String boi = "Please enter a valid operator.";
+	    	return boi;
 	    }
     }
     
+    public static String returner (int[] answer) {
+    	reduce (answer);
+    	if (answer[2] == 1 || answer[1] == 0) { //if the answer is an integer or zero
+    		return Integer.toString(answer[1]);
+    	}
+    	else if (answer[0] != 0) {//if the fraction is improper
+    		String whole = Integer.toString(answer[0]);
+    		String numerator = Integer.toString(answer[1]);
+    		String denominator = Integer.toString(answer[2]);
+    		return whole + " " + numerator + " " + denominator;
+    	}
+    	else {
+    		String numerator = Integer.toString(answer[1]);
+    		String denominator = Integer.toString(answer[2]);
+    		return numerator + " " + denominator;
+    	}
+    }
 	public static int[] operandHolder (String operand) { //returns the numerator, denominator, and whole number for each operand
     	int mixedChecker = operand.indexOf("_"); //checks if the operand is a mixed fraction by searching for '_'
 	    if (mixedChecker == -1) { //if not a mixed fraction...
@@ -117,6 +112,9 @@ public class FracCalc {
     
     public static int[] improper (int [] array1) { //makes a mixed fraction improper and returns an array
     	int numerator = (array1[0] * array1[2]) + array1[1]; //changes numerator so fraction becomes improper
+        if (array1[1] == 1 && array1[2] == 1) {
+		numerator = numerator - 1;
+	}
     	int [] arrayFinal = {0, numerator, array1[2]}; //makes array with whole = 0, new numerator, and original denom.
     	return arrayFinal; //returns array with whole, num, and denom values
     }
@@ -124,6 +122,10 @@ public class FracCalc {
     public static int[] multiply (int[] array1, int[] array2) { //multiplies two operands
     	int numerator = array1[1] * array2[1];
     	int denominator = array1[2] * array2[2];
+    	if (array1[1] == 0 || array2[1] == 0) {
+    		int [] zero = {0, 0, 0};
+    		return zero;
+    	}
     	int[] answer = {0, numerator, denominator};//makes array with final whole, num, and denom values
     	return answer;
     }
@@ -131,6 +133,10 @@ public class FracCalc {
     public static int[] divide (int[] array1, int[] array2) { // divide two operands
     	int num2New = array2[2]; //flips the numerator and denominator
     	int denom2New = array2[1];
+    	if (denom2New < 0) { 
+    		num2New *= -1;
+    		denom2New *= -1;
+    	}
     	array2[1] = num2New; 
     	array2[2] = denom2New;    	
     	int[] answer = multiply (array1, array2); //calls multiply and puts final values in array 'answer'
@@ -167,24 +173,35 @@ public class FracCalc {
     		int divisor = greatestCommonDivisor (array1[1], array1[2]);//finds gcd for the fraction represented by array1
     		array1[1] = array1[1] / divisor;//divides numerator by gcd
     		array1[2] = array1[2] / divisor;//divides denominator by gcd
-    		if (array1[1] > array1[2]) {//if the fraction is improper, turn it into mixed
+    	}
+    	if (Math.abs(array1[1]) > array1[2]) {//if the fraction is improper, turn it into mixed
     			if (array1[2] == 1) {
     				return array1;
+    			}
+    			boolean isNegative = false;
+    			if (array1[1] < 0) {
+    				isNegative = true;
+    				array1[1] *= -1;
     			}
     			while (array1[1] > array1[2]) {
     				array1[1] = array1[1] - array1[2];
     				array1[0]++;
     			}
-    		}
-    		return array1;
+    			if (isNegative == true && array1[0] != 0) {
+    				array1[0] *= -1;
+    			}
+    			else if (isNegative == true) {
+    				array1[1] *= -1;
+    			}
     	}
-    	else {
+    	return array1;
+    	/*else {
     		return array1;
-    	}
+    	}*/
     }
     
     public static boolean canItReduce (int[] array1) { //checks if a fraction can be reduced
-    	for (int i = 2; i <= array1[1]; i++) {
+    	for (int i = 2; i <= Math.abs(array1[1]); i++) {
     		if (array1[1] % i == 0 && array1[2] % i == 0) { //checks which number the num and denom have a common divisor
     			return true;
     		}
@@ -192,9 +209,6 @@ public class FracCalc {
     	return false;
     }
     
-    public static void finalIsInteger (int[] array1) {
-    	
-    }
     /**
      * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
      *      Use this helper method in the Final Checkpoint to reduce fractions.
@@ -215,18 +229,5 @@ public class FracCalc {
             max = tmp;
         }
         return max;
-    }
-    
-    /**
-     * leastCommonMultiple - Find the smallest integer that can be evenly divided by two integers.
-     *      Use this helper method in Checkpoint 3 to evaluate expressions.
-     * @param a - First integer.
-     * @param b - Second integer.
-     * @return The LCM.
-     */
-    public static int leastCommonMultiple(int a, int b)
-    {
-        int gcd = greatestCommonDivisor(a, b);
-        return (a*b)/gcd;
     }
 }
